953128bfbf47f6ef1e23b0c20d66c7cad14ed93a
mohuneko <b0wdmohu@gmail.com>
2020-07-20 20:13:16 +0900

    [add]signupPage

---
+++ db/migrate/20200720105237_add_column_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnToUser < ActiveRecord::Migration[5.2]
+  def change
+
+    add_column :users, :name, :string, null: false, default: '', limimt: 100
+    add_column :users, :self_introduction, :string, limit: 500
+    add_column :users, :sex, :integer, null: false, default: 0
+    add_column :users, :img_name, :string
+  end
+end
